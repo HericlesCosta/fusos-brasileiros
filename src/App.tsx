bb1f120c53570e58a6d406e4528bafeac5d357b7
Hericles <hericlesvitorio@gmail.com>
2023-09-05 11:33:56 -0300

    ajuste das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/brasilia" element={<Brasilia />} />
-        <Route path="/fernando-noronha" element={<FernandoDeNoronha />} />
-        <Route path="/amazonas" element={<Manaus />} />
-        <Route path="/acre" element={<RioBranco />} />
+        <Route path="/fusos-brasileiros" element={<Home />} />
+        <Route path="/fusos-brasileiros/brasilia" element={<Brasilia />} />
+        <Route path="/fusos-brasileiros/fernando-noronha" element={<FernandoDeNoronha />} />
+        <Route path="/fusos-brasileiros/amazonas" element={<Manaus />} />
+        <Route path="/fusos-brasileiros/acre" element={<RioBranco />} />
       </Routes>
     </BrowserRouter>
   )
